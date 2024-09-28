3692165978fb0ca79b90a2d517240fe1b57a0c1a
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-09-28 17:39:24 +0300

    added task-5

--- src/script/countProducts.js
+++ src/script/countProducts.js
@@ -1,1 +1,9 @@
-export const countProducts = async () => {};
+import * as fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products.js';
+
+export const countProducts = async () => {
+  const readProducts = await fs.readFile(PATH_DB);
+  const parsProducts = JSON.parse(readProducts);
+  return parsProducts.length;
+};
+console.log(await countProducts());
